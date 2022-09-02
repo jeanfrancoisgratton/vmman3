95477f60d81d91214c94355b36641964f58287c6
jeanfrancoisgratton <jean-francois@famillegratton.net>
2022-09-02 08:26:31 -0400

    More spec fixes

--- rhel/install-build-deps.sh
+++ rhel/install-build-deps.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-grep ^BuildRequires vmman3.spec |awk -F\: '{print "sudo microdnf install -y"$2}'|sed -e 's/,/ /g' | sh
+echo "Installing BuildRequires dependencies";echo
+grep ^BuildRequires ../vmman3.spec |awk -F\: '{print "sudo microdnf install -y"$2}'|sed -e 's/,/ /g' | sh
+echo;echo;echo "Done. Now installing the Go binaries"
 /opt/bin/install_golang.sh 1.19 amd64
