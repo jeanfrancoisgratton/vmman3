5d50d9661f5539022272635c54cf8bee10453855
builder <builder@famillegratton.net>
2022-09-05 16:12:00 -0400

    Added sudo

--- __debian/1.install-build-deps.sh
+++ __debian/1.install-build-deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Installing dependencies";echo
-apt-get update && apt-get install -y pkg-config libvirt-daemon libvirt-dev gcc
+sudo apt-get update && apt-get install -y pkg-config libvirt-daemon libvirt-dev gcc
 echo;echo;echo "Done. Now installing the Go binaries"
-/opt/bin/install_golang.sh 1.19 amd64
+sudo /opt/bin/install_golang.sh 1.19 amd64
