109cb86a61a423aea0479fb682a83b6b4a79973f
jeanfrancoisgratton <jean-francois@famillegratton.net>
2022-09-17 13:29:22 -0400

    RHEL packaging issue when running from subdir

--- rpm-install-build-deps.sh
+++ rpm-install-build-deps.sh
@@ -3,4 +3,4 @@
 echo "Installing BuildRequires dependencies";echo
 grep ^BuildRequires vmman3.spec |awk -F\: '{print "sudo dnf install -y"$2}'|sed -e 's/,/ /g' | sh
 echo;echo;echo "Done. Now installing the Go binaries"
-/opt/bin/install_golang.sh `grep ^go ../src/go.mod | awk '{print $2}'` amd64
+/opt/bin/install_golang.sh `grep ^go src/go.mod | awk '{print $2}'` amd64
