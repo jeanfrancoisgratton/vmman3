a5d0503c81a62ae056e7ce9200442ba8add68006
jeanfrancoisgratton <jean-francois@famillegratton.net>
2022-10-16 11:44:51 -0400

    Added file needed to build

---
+++ src/helpers/vmHelpers.go
@@ -0,0 +1,9 @@
+// vmman3 : Écrit par Jean-François Gratton (jean-francois@famillegratton.net)
+// src/helpers/vmHelpers.go
+// 2022-10-16 10:08:00
+
+package helpers
+
+func VMstateChange(hypervisor string, vmname string) {
+	
+}
